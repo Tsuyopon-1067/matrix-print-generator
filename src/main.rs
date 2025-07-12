9b194a19ed34b92d198b3dd3c3c5ec38e6193f56
Tsuyopon-1067 <91947809+Tsuyopon-1067@users.noreply.github.com>
2025-07-13 02:04:15 +0900

    ✨ allow to generate sub and mul

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use matrix_print_generator::typst_generator;
 fn main() {
     let mut problems = Vec::new();
     for _ in 0..500 {
-        problems.push(Problem::new_add(3, 3, 0, 10));
+        problems.push(Problem::new_mul(3, 3, 3, 0, 10));
     }
 
     let result = typst_generator::generate_typst_file(&problems);
--- src/problem.rs
+++ src/problem.rs
@@ -4,7 +4,8 @@ use crate::matrix::Matrix;
 #[derive(Debug, Clone, Copy)]
 pub enum Operation {
     Add,
-    // Mul,
+    Sub,
+    Mul,
 }
 
 // 問題の構造体
@@ -34,6 +35,50 @@ impl Problem {
         }
     }
 
+    // 新しい引き算問題を生成
+    pub fn new_sub(rows: usize, cols: usize, min_val: i32, max_val: i32) -> Self {
+        let m1 = Matrix::new_random(rows, cols, min_val, max_val);
+        let m2 = Matrix::new_random(rows, cols, min_val, max_val);
+        let mut answer_data = vec![vec![0; cols]; rows];
+        for i in 0..rows {
+            for j in 0..cols {
+                answer_data[i][j] = m1.data[i][j] - m2.data[i][j];
+            }
+        }
+        let answer = Matrix::new(rows, cols, answer_data);
+        Self {
+            operands: vec![m1, m2],
+            answer,
+            op: Operation::Sub,
+        }
+    }
+
+    // 新しい掛け算問題を生成
+    pub fn new_mul(
+        rows: usize,
+        common_dim: usize,
+        cols: usize,
+        min_val: i32,
+        max_val: i32,
+    ) -> Self {
+        let m1 = Matrix::new_random(rows, common_dim, min_val, max_val);
+        let m2 = Matrix::new_random(common_dim, cols, min_val, max_val);
+        let mut answer_data = vec![vec![0; cols]; rows];
+        for i in 0..rows {
+            for j in 0..cols {
+                for k in 0..common_dim {
+                    answer_data[i][j] += m1.data[i][k] * m2.data[k][j];
+                }
+            }
+        }
+        let answer = Matrix::new(rows, cols, answer_data);
+        Self {
+            operands: vec![m1, m2],
+            answer,
+            op: Operation::Mul,
+        }
+    }
+
     // Typst形式の文字列に変換
     pub fn to_typst(&self) -> String {
         let mut s = "".to_string();
@@ -42,6 +87,8 @@ impl Problem {
             if i < self.operands.len() - 1 {
                 match self.op {
                     Operation::Add => s.push_str(" + "),
+                    Operation::Sub => s.push_str(" - "),
+                    Operation::Mul => s.push_str(" $times$ "),
                 }
             }
         }
